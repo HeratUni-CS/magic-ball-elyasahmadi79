4c28fd2dbb994e4129621e0b45b0503fbc037ce0
Elyasahmadi79 <elyas.ahmady1@gmail.comm>
2023-06-17 14:08:10 +0430

    Magic Ball Project All Sections Done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,46 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: null,
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BallPage(),
     ),
   );
+}
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[800],
+      appBar: AppBar(
+        title: Text("Ask me Some questions !"),
+        centerTitle: true,
+        backgroundColor: Colors.orange[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset("images/ball$ballNumber.png"),
+        onPressed: () {
+          print('I got clicked');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
 }
